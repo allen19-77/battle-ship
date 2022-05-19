d11e798f3f40557c1d2e9ee0eb2390448b88714c
allen19-77 <allen.19@list.ru>
2022-05-19 20:41:19 +0500

    first commit

---
+++ lessons.js
@@ -0,0 +1,13 @@
+const [lBoard, rBoard] = document.querySelectorAll('.board')
+const startGameBtn = document.querySelector('#start-btn')
+
+startGameBtn.onclick = startGame
+
+function startGame() {
+	lBoard.onclick = 
+}
+
+function toggleShipDeck() {
+	
+}
+
